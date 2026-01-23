6c78e343a8db7d6895b4b9c5835d470a8974a401
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-23 20:34:48 +0100

    update object creation smoke test

--- SmokeTests/ObjectTestController.cs
+++ SmokeTests/ObjectTestController.cs
@@ -11,16 +11,53 @@ namespace LunyScript.Godot.Tests
 		[Export] private Boolean _CubeObjectCreated;
 		[Export] private Boolean _SphereObjectCreated;
 
-		private Boolean _foundToBeDestroyedObject;
+		private Boolean _foundToBeDestroyedNode;
+
+		private Int32 processCount;
+
+		private static SceneTree SceneTree => (SceneTree)Engine.GetMainLoop();
 
 		public override void _Ready()
 		{
 			var node = new Node();
-			node.Name = nameof(ObjectTestLunyScript);
+			node.Name = nameof(ObjectTestLunyScript) + "RuntimeCreated_(NotImplemented)";
+			//var godotNode = new GodotNode(node);
 
 			// this causes the corresponding LunyScript to build & run
-			var sceneTree = (SceneTree)Engine.GetMainLoop();
-			sceneTree.CurrentScene.CallDeferred("add_child", node);
+			SceneTree.CurrentScene.CallDeferred("add_child", node);
+		}
+
+		public override void _Process(Double delta)
+		{
+			if (!_foundToBeDestroyedNode)
+			{
+				var rootNodes = SceneTree.CurrentScene.GetChildren();
+				foreach (var rootNode in rootNodes)
+				{
+					if (rootNode.Name == ObjectTestLunyScript.DestroyedObjectName)
+						_foundToBeDestroyedNode = true;
+				}
+			}
+
+			processCount++;
+			if (processCount == 2)
+				AssertObjectsCreated();
+		}
+
+		private void AssertObjectsCreated()
+		{
+			var destroyedNameFound = false;
+			var rootNodes = SceneTree.CurrentScene.GetChildren();
+			foreach (var rootNode in rootNodes)
+			{
+				var nodeName = rootNode.Name;
+				_EmptyObjectCreated = _EmptyObjectCreated || nodeName == ObjectTestLunyScript.EmptyObjectName;
+				_CubeObjectCreated = _CubeObjectCreated || nodeName == ObjectTestLunyScript.CubeObjectName;
+				_SphereObjectCreated = _SphereObjectCreated || nodeName == ObjectTestLunyScript.SphereObjectName;
+				destroyedNameFound = destroyedNameFound || nodeName == ObjectTestLunyScript.DestroyedObjectName;
+			}
+
+			_ObjectCreatedAndDestroyed = _foundToBeDestroyedNode && destroyedNameFound == false;
 		}
 	}
 }
